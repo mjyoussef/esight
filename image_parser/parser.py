90041041e656364a366dfec90533b7ab4b0032e6
Michael Youssef <michael_youssef@brown.edu>
2022-07-19 17:12:35 -0400

    updated parser

--- image_parser/parser.py
+++ image_parser/parser.py
@@ -1,34 +1,51 @@
 from PIL import Image
 import numpy as np
-import initializer as init
+
+def to_array(file):
+    img = Image.open(file)
+    return np.asarray(img)
+
+def contrast(p1, p2):
+    return (0.3 * p1[0] + 0.59 * p1[1] + 0.11 * p1[2]) \
+        - (0.3 * p2[0] + 0.59 * p2[1] + 0.11 * p2[2])
+
+def createEmptyMap(len, width, val):
+    map = []
+    for i in range(len):
+        row = []
+        for j in range(width):
+            row.append(val)
+        map.append(row)
+    return map
 
 class Parser:
     def __init__(self, image, base, border_color, threshold):
-        self.image = image
-        self.base = base
-        self.annotated_img = init.initializeImg(image, base)
+        self.image = to_array(image)
+        self.base = to_array(base)
+        self.visited = createEmptyMap(len(image), len(image[0]), False)
         self.border_color = border_color
         self.threshold = threshold
     
     def parse(self):
-        for i in range(len(self.annotated_img)):
-            for j in range(len(self.annotated_img[i])):
-                if (self.annotated_img[i][j].contrast >= self.threshold):
-                    self.search(i, j)
+        for i in range(len(self.image)):
+            for j in range(len(self.image[i])):
+                if (not self.visited[i][j] and contrast(self.image[i][j], self.base[i][j]) >= self.threshold):
+                    temp = createEmptyMap(len(self.image), len(self.image[0]), 0)
+                    self.search(i, j, temp)
 
-    def search(self, row, column):
-        if self.annotated_img[row][column].seen:
-            pass
-        elif self.annotated_img.contrast >= self.threshold:
-            if row != 0:
-                self.search(row - 1, column)
-            if row != len(self.annotated_img) - 1:
-                self.search(row + 1, column)
-            if column != 0:
-                self.search(row, column - 1)
-            if column != len(self.annotated_img[0]) - 1:
-                self.search(row, column + 1)
-        else:
-            self.base[row][column] = self.border_color
-        
-        self.annotated_img[row][column].seen = True
+                    # find a convex hull over the border regions in temp
+                    # update the image using the convex hull outline in temp
+
+
+    def search(self, row, column, map):
+        if map[row][column] == 0:
+            if contrast(self.image[row][column], self.base[row][column]) >= self.threshold:
+                map[row][column] = 1
+                if row > 0:
+                    self.search(row - 1, column, map)
+                if row < len(self.image) - 1:
+                    self.search(row + 1, column, map)
+                if column > 0:
+                    self.search(row, column - 1, map)
+                if column < len(self.image[0]) - 1:
+                    self.search(row, column + 1, map)
